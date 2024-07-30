6b97624a44e5a2a51687b8fb8baa27fe40767eb1
stuepvfx x <efrizalrajomudo@gmail.com>
2024-07-30 01:44:46 +0000

    g

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-const PAYHIP_API_KEY = '059b33d562e1d458ed9e6de438cb538d92508b38';
+// config.js
+const API_KEY = '059b33d562e1d458ed9e6de438cb538d92508b38';
+const API_URL = 'https://api.payhip.com/products'; // Ganti dengan endpoint API yang sesuai
